635de97244d24cbc3c6febd32c9ed99d4e97dd2c
Wissarut Kanasub <qosanglesz@gmail.com>
2022-12-11 15:43:52 +0700

    Add slide and delete tested file

--- Tested.py
+++
@@ -1,9 +0,0 @@
-import os
-from collections import Counter
-data = {'Eusebio': 120, 'Cruyff': 104, 'Pele': 150, 'Ronaldo': 132, 'Messi': 125}
-sorted_data = sorted(data.items(), key=lambda x: x[1], reverse=True)
-convected_data = dict(sorted_data)
-print(convected_data)
-top3 = dict(Counter(convected_data).most_common(3))
-print(top3)
-
